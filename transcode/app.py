ff3f83c84978adba40cf6bf4dcd148d8c90c15da
Kishore Jagannath <kishorerj@google.com>
2022-06-09 08:57:02 +0530

    removed health method

--- transcode/app.py
+++ transcode/app.py
@@ -4,21 +4,16 @@ import base64
 import transcode_job
 from google.cloud import logging
 from flask import Flask, request
-from key import apikey
+
 
 
 app = Flask(__name__)
 
 
-@app.route('/health')
-def health():
+@app.route('/test')
+def test():
     return 'It is alive!\n'
 
-
-@app.route('/secure')
-@apikey
-def health():
-    return 'I am secure!'
 
 @app.route("/", methods=["POST"])
 def index():
@@ -35,9 +30,7 @@ def index():
 
     pubsub_message = envelope["message"]
     print(pubsub_message)
-    
     client = logging.Client()
-
     logger = client.logger("service_1")
     logger.log(pubsub_message)
     
